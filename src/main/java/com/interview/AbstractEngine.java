f6ec2fc5332a1994a740515a84171a9c8bf8c23f
arun7 <po7456@yahoo.com>
2016-05-01 17:29:02 +0100

    Reactored to include an abstract engine class

---
+++ src/main/java/com/interview/AbstractEngine.java
@@ -0,0 +1,63 @@
+package com.interview;
+
+/**
+ * Created on 01/05/2016.
+ */
+public abstract class AbstractEngine implements Engine{
+
+    protected boolean running;
+    protected int fuelLevel;
+    protected FuelType requiredFuelType;
+    protected FuelType fuelType;
+
+    /**
+     * Constructor
+     * @param requiredFuelType the required fuel type accepted by engine
+     */
+    public AbstractEngine(FuelType requiredFuelType){
+        this.requiredFuelType = requiredFuelType;
+        running = false;
+        fuelLevel = 0;
+    }
+
+    public void start() {
+        if (fuelLevel > 0 && requiredFuelType.equals(fuelType)) {
+            running = true;
+        } else {
+            throw new IllegalStateException("Not able to start engine.");
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void fill(FuelType fuelType, int fuelLevel) {
+        if (fuelLevel >= 0 && fuelLevel <= 100) {
+            this.fuelLevel = fuelLevel;
+        }
+        else if (fuelLevel > 100) {
+            this.fuelLevel = 100;
+        }
+        else {
+            this.fuelLevel = 0;
+        }
+
+        this.fuelType = fuelType;
+    }
+
+    public FuelType getFuelType() {
+        return  requiredFuelType;
+    }
+
+    public int getFuelLevel(){
+        return this.fuelLevel;
+    }
+
+    public abstract int getProduceCost(int quantity);
+
+}
--- src/main/java/com/interview/InternalCombustionEngine.java
+++ src/main/java/com/interview/InternalCombustionEngine.java
@@ -1,50 +1,21 @@
 package com.interview;
 
-public class InternalCombustionEngine implements Engine{
+public class InternalCombustionEngine extends AbstractEngine{
 
-    private boolean running;
-    private int fuelLevel;
-    private FuelType requiredFuelType;
-    private FuelType fuelType;
-
+    /**
+     * Constructor
+     * @param requiredFuelType the required fuel type
+     */
     public InternalCombustionEngine(FuelType requiredFuelType) {
-        this.requiredFuelType = requiredFuelType;
-        running = false;
-        fuelLevel = 0;
-    }
-
-    public void start() {
-        if (fuelLevel > 0 && requiredFuelType.equals(fuelType)) {
-            running = true;
-        } else {
-            throw new IllegalStateException("Not able to start engine.");
-        }
-    }
-
-    public void stop() {
-        running = false;
-    }
-
-    public boolean isRunning() {
-        return running;
+        super(requiredFuelType);
     }
 
     public void fill(FuelType fuelType, int fuelLevel) {
-        if (fuelLevel >= 0 && fuelLevel <= 100) {
-            this.fuelLevel = fuelLevel;
+        if(requiredFuelType==fuelType){
+            if(fuelType==FuelType.DIESEL || fuelType==FuelType.PETROL){
+                super.fill(fuelType, fuelLevel);
+            }
         }
-        else if (fuelLevel > 100) {
-            this.fuelLevel = 100;
-        }
-        else {
-            this.fuelLevel = 0;
-        }
-
-        this.fuelType = fuelType;
-    }
-
-    public FuelType getFuelType() {
-        return  requiredFuelType;
     }
 
     public int getProduceCost(int quantity){
@@ -65,9 +36,4 @@ public class InternalCombustionEngine implements Engine{
 
         return batchCount * costPerBatch;
     }
-
-    public int getFuelLevel(){
-        return this.fuelLevel;
-    }
-
 }
