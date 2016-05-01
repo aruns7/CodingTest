09b595362b59f21a03987235434fa9ee3d6e8d55
arun7 <po7456@yahoo.com>
2016-05-01 16:35:46 +0100

    Initial commit

---
+++ src/test/java/com/interview/WidgetMachineTest.java
@@ -0,0 +1,15 @@
+package com.interview;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created on 01/05/2016.
+ */
+public class WidgetMachineTest {
+
+    @Test
+    public void widget(){
+        assertTrue(true);    }
+
+}
